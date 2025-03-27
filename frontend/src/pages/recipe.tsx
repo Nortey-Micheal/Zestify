06638c5b3d3c9ec630935b9171d58f2c56d57718
Nortey Michael <norteymicheal91@gmail.com>
2025-03-27 13:20:48 +0000

    fix: fetching of recipes should start with a local serach before making api call

--- frontend/src/pages/recipe.tsx
+++ frontend/src/pages/recipe.tsx
@@ -1,18 +1,36 @@
 import useGetRecipeById from "@/hooks/recipes/useGetRecipeById"
+import { setRecipePage } from "@/redux/recipe/recipePageSlice"
 import { RootState } from "@/redux/store"
 import { useEffect } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router"
 
 export default function Recipe() {
     const recipe_id = useParams<{recipe_id:string}>()
     const recipe = useSelector((state:RootState) => state.recipePage)
+    const newRecipes = useSelector((state:RootState) => state.newRecipes)
+    const popularRecipes = useSelector((state:RootState) => state.popularRecipes)
     const { getRecipeById } = useGetRecipeById()
+    const dispatch = useDispatch()
 
     useEffect(() => {
         const fetchRecipe = async () => {
-            alert(recipe_id)
-            await getRecipeById(recipe_id.recipe_id!)
+           const recipeFromNew = newRecipes.find(recipe => recipe_id.recipe_id === recipe._id)
+           let recipeFromPopular;
+
+           if (!recipeFromNew) {
+                recipeFromPopular = popularRecipes.find(recipe => recipe_id.recipe_id === recipe._id)
+           } else {
+                dispatch(setRecipePage(recipeFromNew))
+           }
+
+           if (!recipeFromPopular || !recipeFromNew){
+                await getRecipeById(recipe_id.recipe_id!)
+                console.log(recipeFromNew, recipeFromPopular)
+           } else {
+                dispatch(setRecipePage(recipeFromPopular))
+           }
+           
         }
         fetchRecipe()
     },[])
