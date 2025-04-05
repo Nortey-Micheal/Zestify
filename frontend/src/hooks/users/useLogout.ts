f71c25940fff2dabb287fc385b77cf14cc84389b
Nortey Michael <norteymicheal91@gmail.com>
2025-04-05 19:36:53 +0000

    `Added calls to getNewRecipes and getPopularRecipes in useLogout hook`

--- frontend/src/hooks/users/useLogout.ts
+++ frontend/src/hooks/users/useLogout.ts
@@ -3,15 +3,21 @@ import { removeAuthentication } from "@/redux/user/isAuthenticated"
 import { removeUser } from "@/redux/user/userSlice"
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router"
+import useGetNewRecipes from "../recipes/useGetNewRecipes"
+import useGetPopularRecipes from "../recipes/useGetPopularRecipes"
 
 const useLogOut = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const { getNewRecipes } = useGetNewRecipes()
+    const { getPopularRecipes } = useGetPopularRecipes()
     
     const logout = async() => {
         await persistor.purge()
         dispatch(removeUser())
         dispatch(removeAuthentication())
+        await getNewRecipes()
+        await getPopularRecipes()
         navigate('/')
     }
 
