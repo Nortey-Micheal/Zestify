b5376d430315a46b6246aa84c419f6e21a5b6f00
Nortey Michael <norteymicheal91@gmail.com>
2025-04-25 12:15:34 +0000

    refactor: rename loading state variable in PopularRecipes component

--- frontend/src/pages/recipe/viewRecipe/popularRecipes.tsx
+++ frontend/src/pages/recipe/viewRecipe/popularRecipes.tsx
@@ -8,7 +8,7 @@ import { toast } from "sonner"
 
 export default function PopularRecipes() {
     const popularRecipes = useSelector((state:RootState) => state.popularRecipes)
-    const { getPopularRecipes, error, isLoading } = useGetPopularRecipes()
+    const { getPopularRecipes, error, isPopularRecipeLoading } = useGetPopularRecipes()
     const [page,setPage] = useState<number>(1)
 
     useEffect(() => {
@@ -37,13 +37,13 @@ export default function PopularRecipes() {
                     <button disabled={page === 1} className="flex items-center gap-2 text-xl hover:bg-(--zesty-orange) hover:text-(--white) font-bold cursor-pointer rounded-lg disabled:bg-(--light-gray) px-2 my-1 disabled:text-(--light-grey) " onClick={() => setPage(prev => Math.max((prev - 1), 1))}><ArrowBigLeftDashIcon /> Prev</button>
                 </li>
                 <li>
-                    <button className="flex items-center gap-2 text-xl hover:bg-(--zesty-orange) hover:text-(--white) font-bold cursor-pointer rounded-lg disabled:bg-(--light-gray) px-2 my-1 disabled:text-(--light-grey) " disabled={(popularRecipes.length < 6) || isLoading} onClick={() => setPage(prev => Math.max((prev + 1), 1))}>Next
+                    <button className="flex items-center gap-2 text-xl hover:bg-(--zesty-orange) hover:text-(--white) font-bold cursor-pointer rounded-lg disabled:bg-(--light-gray) px-2 my-1 disabled:text-(--light-grey) " disabled={(popularRecipes.length < 6) || isPopularRecipeLoading} onClick={() => setPage(prev => Math.max((prev + 1), 1))}>Next
                     <ArrowBigRightDashIcon />
                     </button>
                 </li>
             </ul>
             {
-               isLoading && (
+               isPopularRecipeLoading && (
                 <div className="bg-(--dark-charcoal) rounded-2xl h-full w-full absolute left-0 top-0 flex items-center justify-center ">
                   <LoaderPinwheel className=" w-25 h-25"/>
                 </div>
